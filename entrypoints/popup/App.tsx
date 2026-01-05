b0b05380bf809456b34e9b469b0d6a1c452dc644
Vahid <vahidmenu@gmail.com>
2026-01-05 21:46:08 +0300

    init

---
+++ entrypoints/popup/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "@/assets/react.svg";
+import wxtLogo from "/wxt.svg";
+import "./AppTailwind.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">Hello World</div>
+    </>
+  );
+}
+
+export default App;
