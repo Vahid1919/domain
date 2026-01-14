e807d0e988169843483cc721dfe75bef6c9e947f
Vahid <vahidmenu@gmail.com>
2026-01-15 00:17:33 +0300

    feat: add tailwind + shadcn

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,13 +1,29 @@
 import { useState } from "react";
-import reactLogo from "@/assets/react.svg";
-import wxtLogo from "/wxt.svg";
-import "./AppTailwind.css";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <>
-      <div className="container">Hello World</div>
-    </>
+    <div className="w-96 p-2 space-y-4 bg-white rounded-md shadow-md">
+      <h1 className="scroll-m-20 text-left text-4xl font-extrabold tracking-tight text-balance">
+        Lamzu
+      </h1>
+      <h3 className="text-muted-foreground text-sm">
+        Less Distraction, More Creation
+      </h3>
+
+      <Tabs defaultValue="account" className="w-full m-5">
+        <TabsList className="p-2">
+          <TabsTrigger value="limit">Set Limits</TabsTrigger>
+          <TabsTrigger value="block">Blocked Sites</TabsTrigger>
+          <TabsTrigger value="accountability">Accountability</TabsTrigger>
+        </TabsList>
+        <TabsContent value="limit">Limit</TabsContent>
+        <TabsContent value="block">Block</TabsContent>
+        <TabsContent value="accountability">Accountability</TabsContent>
+      </Tabs>
+    </div>
   );
 }
 
